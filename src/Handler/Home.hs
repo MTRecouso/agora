157160328ab72547f3e05d3579d2704276a4f7af
mateus <mateus.recouso@hotmail.com>
2018-05-11 12:12:53 -0300

    created database models, added connection to heroku db, created routes for articles

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -11,3 +11,25 @@ import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
 getHomeR = undefined
+
+
+postUserSyR :: Handler Value
+postUserSyR = do
+    user <- requireJsonBody :: Handler UserSy
+    uid <- runDB $ insert user
+    sendStatusJSON created201 (object["resp" .= fromSqlKey uid])
+
+
+postArticleR :: Handler Value
+postArticleR = do
+    article <- requireJsonBody :: Handler Article
+    aid <- runDB $ insert article
+    sendStatusJSON created201 (object["resp" .= fromSqlKey aid])
+
+
+getArticleByIdR :: ArticleId -> Handler Value
+getArticleByIdR articleId = do
+    article <- runDB $ get404 articleId
+    listArticleReactions <- runDB $ selectList [ReactionArticle ==. articleId] []
+    listArticleViews <- runDB $ selectList [ViewArticle ==. articleId] []
+    sendStatusJSON ok200 (object["resp" .= (article, listArticleViews, listArticleReactions)])
