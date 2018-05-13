27f49970e10084542ba4f4bf6ffe731b1f04a79a
mateus <mateus.recouso@hotmail.com>
2018-05-13 19:18:24 -0300

    Added POST and GET routes for Reactions and Views.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -33,3 +33,28 @@ getArticleByIdR articleId = do
     listArticleReactions <- runDB $ selectList [ReactionArticle ==. articleId] []
     listArticleViews <- runDB $ selectList [ViewArticle ==. articleId] []
     sendStatusJSON ok200 (object["resp" .= (article, listArticleViews, listArticleReactions)])
+
+postReactionR :: Handler Value
+postReactionR = do
+    reaction <- requireJsonBody :: Handler Reaction
+    rid <- runDB $ insert reaction
+    sendStatusJSON created201 (object["resp" .= fromSqlKey rid])
+
+
+getReactionByIdR :: ReactionId -> Handler Value
+getReactionByIdR reactionId = do
+    reaction <- runDB $ get404 reactionId
+    sendStatusJSON ok200 (object["resp" .= (reaction)])
+
+
+postViewR :: Handler Value
+postViewR = do
+    view <- requireJsonBody :: Handler View
+    vid <- runDB $ insert view
+    sendStatusJSON created201 (object["resp" .= fromSqlKey vid])
+
+
+getViewByIdR :: ViewId -> Handler Value
+getViewByIdR viewId = do
+    view <- runDB $ get404 viewId
+    sendStatusJSON ok200 (object["resp" .= (view)])
