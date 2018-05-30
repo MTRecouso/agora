f024024ef69639d8a1a53a6174918250c0a4e2a6
leandroof <leandro08pf@outlook.com>
2018-05-30 13:08:12 -0300

    Added POST route for registering users. Password hashing is made with bcrypt package

--- src/Handler/User.hs
+++ src/Handler/User.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.User where
@@ -8,12 +9,21 @@ module Handler.User where
 import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import qualified Data.ByteString.Char8 as BS (pack, unpack)
+import Crypto.BCrypt
+--import Yesod.Auth.HashDB (setPassword,userPasswordHash, setPasswordHash, validatePass, HashDBUser)
+--import Yesod.Auth.Util.PasswordStore (makePassword, strengthenPassword,
+                                             -- verifyPassword, passwordStrength)
+
+
 
 postUserSyR :: Handler Value
 postUserSyR = do
     user <- requireJsonBody :: Handler UserSy
-    uId <- runDB $ insert user
-    sendStatusJSON created201 (object["user" .= fromSqlKey uId])
+    (Just hashedPass) <- liftIO $ hashPasswordUsingPolicy slowerBcryptHashingPolicy (BS.pack $ unpack $ userSyPassword user)
+    hashUser <- return $ UserSy (userSyEmail user) (pack $ BS.unpack hashedPass) (userSyUsername user)
+    userId <- runDB $ insert hashUser
+    sendStatusJSON created201 (object ["resp" .= (userId)])
 
 getUserSyByIdR :: UserSyId -> Handler Value
 getUserSyByIdR uId = do
