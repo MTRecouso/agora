3c65a646c60a31289aa8620fd2bc20630542006f
leandroof <leandro08pf@outlook.com>
2018-06-02 19:33:37 -0300

    Created login page on GET route 'LoginPageR'

--- src/Handler/User.hs
+++ src/Handler/User.hs
@@ -51,10 +51,14 @@ postUserLoginR = do
 
 getLoginPageR :: Handler Html
 getLoginPageR = do
+    pc <- return $ $(widgetFile "login")
     defaultLayout $ do
-        [whamlet|
-            <h1>TESTE
-        |]
+        addStylesheetRemote "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css"
+        addStylesheetRemote "https://fonts.googleapis.com/icon?family=Material+Icons"
+        addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"
+        addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"
+        setTitle "Ágora"
+        $(widgetFile "layoutlogin")
    
 
 getUserSyByIdR :: UserSyId -> Handler Value
